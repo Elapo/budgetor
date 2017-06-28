2c3a1ad52b409aed186ae0ee9217d84195974738
Frederik Van Herbruggen <Frederik.vanh@gmail.com>
2017-06-28 23:24:55 +0200

    added start scripts & fixed mail dep

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -Dspring.profiles.active=local -Xms32M -Xmx64M -XX:MaxMetaspaceSize=64M -server -jar budgetor-1.0-SNAPSHOT.jar
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Dspring.profiles.active=remote -Xms32M -Xmx64M -XX:MaxMetaspaceSize=64M -server -jar budgetor-1.0-SNAPSHOT.jar
